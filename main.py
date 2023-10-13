d51ecda576690f62a099fb9891b64cf6659766bb
Derrick wakhu shibero <98383475+blackwakhu@users.noreply.github.com>
2023-10-13 10:51:16 +0300

    getting the data using a select element

--- main.py
+++ main.py
@@ -5,13 +5,24 @@ from bs4 import BeautifulSoup
 use requests to download the url 
 the syntax is 
   response = request.get(url)
-now we are going to use a simple url 'https://scrapme.shop/live/'
+now we are going to use a simple url 'https://scrapeme.shop/live/'
 """
 
-response = requests.get("https://scrapme.shop/live/")
+response = requests.get("https://scrapeme.shop/live/")
 
 """
 we are going to use beautiful soup to get the html content from requests.content
 """
 
 soup = BeautifulSoup(response.content, "html.parser")
+
+"""create a selector for a css attribute"""
+link_elems = soup.select("a[href]")
+
+"""get the urls"""
+urls = []
+
+for link_elem in link_elems:
+  url = link_elem["href"]
+  if "https://scrapeme.live/" in url:
+    urls.append(url)
