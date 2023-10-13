d97701a6e5ef627a729e3b5a677bc8295e09b6e2
Derrick wakhu shibero <98383475+blackwakhu@users.noreply.github.com>
2023-10-13 10:35:15 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,2 +1,11 @@
 import requests
 from bs4 import BeautifulSoup
+
+"""
+use requests to download the url 
+the syntax is 
+  response = request.get(url)
+now we are going to use a simple url 'https://scrapme.shop/live/'
+"""
+
+response = requests.get("https://scrapme.shop/live/")
